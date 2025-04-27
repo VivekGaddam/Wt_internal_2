717560863b919692ff97d28214c2f65f60c9b1a8
Vivek Gaddam <154349504+VivekGaddam@users.noreply.github.com>
2025-04-27 20:41:13 +0530

    Update App.js

--- question8/src/App.js
+++ question8/src/App.js
@@ -1,29 +1,16 @@
 import React, { Component } from 'react';
-import LifecycleDemo from './LifecycleDemo.js';  // Import the component
+import LifecycleDemo from './LifecycleDemo'; // Importing the component
 import './App.css';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      showLifecycleDemo: true,
-    };
-  }
-
-  toggleLifecycleDemo = () => {
-    this.setState({ showLifecycleDemo: !this.state.showLifecycleDemo });
-  }
-
   render() {
     return (
       <div className="App">
         <h1>React Lifecycle Methods Example</h1>
-        <button onClick={this.toggleLifecycleDemo} style={{ padding: "8px 12px" }}>
-          Toggle Lifecycle Demo Component
-        </button>
-        {this.state.showLifecycleDemo && <LifecycleDemo />}
+        <LifecycleDemo />
       </div>
     );
   }
 }
+
 export default App;
